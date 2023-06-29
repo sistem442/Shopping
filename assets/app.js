abcb8ee03993704c333323b8182ada1da2c29723
boris <boris.klinko@gmail.com>
2023-06-29 12:23:03 +0200

    List is working

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,10 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
+/* any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 import $ from 'jquery';
 import greet from './greet';
 $(document).ready(function() {
     $('body').prepend('<h1>'+greet('jill')+'</h1>');
-});
+});*/
